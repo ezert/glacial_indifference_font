9d4d45b742d9669c4c20a8e16125b546e4dc5a33
Ezert <ezert.mail@gmail.com>
2023-12-12 03:12:44 -0300

    missing name field __manifest.py__

--- __manifest__.py
+++ __manifest__.py
@@ -1,5 +1,9 @@
-'assets': {
-    'web._assets_primary_variables': [
-        "glacial_indifference_font/static/src/scss/map-merge.scss"
-    ]
+
+{
+    'name': "Glacial Indifference Font",
+    'assets': {
+        'web._assets_primary_variables': [
+            "glacial_indifference_font/static/src/scss/map-merge.scss"
+        ]
+    },
 }
