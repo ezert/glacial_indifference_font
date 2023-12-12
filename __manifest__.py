e5047c8eaa383a4e4111640f7cd27d1cc1606f1b
Ezert <ezert.mail@gmail.com>
2023-12-12 03:52:31 -0300

    infos __manifest.py__

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,10 @@
 
 {
     'name': "Glacial Indifference Font",
+    'version': '1.0',
+    'description': 'Adds the Glacial Indifference Font to the Website builder',
+    'summary': 'Glacial Indifference Font',
+    'author': 'ezert',
     'assets': {
         'web._assets_primary_variables': [
             "glacial_indifference_font/static/src/scss/map-merge.scss"
